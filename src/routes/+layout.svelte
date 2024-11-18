60870574df43739ba8060e412043fd9cf678d873
zxTyphoon <seantylerstraub@gmail.com>
2024-11-18 09:23:16 +0100

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,64 @@
+<script>
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Highlight JS
+	import hljs from 'highlight.js/lib/core';
+	import 'highlight.js/styles/github-dark.css';
+	import { storeHighlightJs } from '@skeletonlabs/skeleton';
+	import xml from 'highlight.js/lib/languages/xml'; // for HTML
+	import css from 'highlight.js/lib/languages/css';
+	import javascript from 'highlight.js/lib/languages/javascript';
+	import typescript from 'highlight.js/lib/languages/typescript';
+
+	hljs.registerLanguage('xml', xml); // for HTML
+	hljs.registerLanguage('css', css);
+	hljs.registerLanguage('javascript', javascript);
+	hljs.registerLanguage('typescript', typescript);
+	storeHighlightJs.set(hljs);
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Jue (ü?) rgen J Straub</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://www.instagram.com/jjstraub1"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Instagram
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://www.facebook.com/JJStraub/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Facebook
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://www.filmmakers.eu/de/actors/j-j-straub"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Filmmakers Profile
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
